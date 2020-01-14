e3a6857c42724546348fec820c23c65ec52c069e
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 14:10:50 +0300

    added the getter methods

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -1,4 +1,17 @@
 package models;
 
 public class EndangeredAnimals {
+    private int animal_id;
+    private String animal_name;
+    private String animal_health;
+    private String animal_age;
+    private int id;
+
+    public EndangeredAnimals(String animal_name,String animal_health, String animal_age ){
+        this.animal_name = animal_name;
+        this.animal_health = animal_health;
+        this.animal_age = animal_age;
+        this.id = 1;
+    }
+
 }
