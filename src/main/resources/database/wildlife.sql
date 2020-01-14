8198deb9a7b39c053c066f891cfe24c870112503
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 11:08:10 +0300

    adjusted the animals variables and getters

--- src/main/resources/database/wildlife.sql
+++ src/main/resources/database/wildlife.sql
@@ -5,3 +5,22 @@ CREATE TABLE IF NOT EXISTS animals (
   animals_id int,
   name VARCHAR
   );
+
+CREATE TABLE IF NOT EXISTS endangered_animals(
+  id int PRIMARY KEY auto_increment,
+  animals_id int,
+  animals_name VARCHAR,
+  animals_age int,
+  animals_health VARCHAR
+):
+
+CREATE TABLE IF NOT EXISTS  sightings(
+   id int PRIMARY KEY auto_increment,
+   animals_id int,
+   animals_name VARCHAR,
+   animals_location VARCHAR,
+   ranger_name VARCHAR,
+   created_at TIMESTAMP
+);
+
+
