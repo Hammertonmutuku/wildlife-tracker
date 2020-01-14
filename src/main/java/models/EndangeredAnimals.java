514364dcaee4206cb57cf8ff05ec762220d6ffae
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 14:19:06 +0300

    added override

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -1,43 +1,43 @@
 package models;
 
 public class EndangeredAnimals {
-    private int animal_id;
-    private String animal_name;
-    private String animal_health;
-    private String animal_age;
+    private int animals_id;
+    private String animals_name;
+    private String animals_health;
+    private String animals_age;
     private int id;
 
     public EndangeredAnimals(String animal_name,String animal_health, String animal_age ){
-        this.animal_name = animal_name;
-        this.animal_health = animal_health;
-        this.animal_age = animal_age;
+        this.animals_name = animal_name;
+        this.animals_health = animal_health;
+        this.animals_age = animal_age;
         this.id = 1;
     }
-    public int getAnimal_id() {
-        return animal_id;
+    public int getAnimals_id() {
+        return animals_id;
     }
 
-    public void setAnimal_id(int animal_id) {
-        this.animal_id = animal_id;
+    public void setAnimals_id(int animals_id) {
+        this.animals_id = animals_id;
     }
 
-    public String getAnimal_name() {
-        return animal_name;
+    public String getAnimals_name() {
+        return animals_name;
     }
 
-    public void setAnimal_name(String animal_name) {
-        this.animal_name = animal_name;
+    public void setAnimals_name(String animals_name) {
+        this.animals_name = animals_name;
     }
 
-    public String getAnimal_health() {
-        return animal_health;
+    public String getAnimals_health() {
+        return animals_health;
     }
-    public String getAnimal_age() {
-        return animal_age;
+    public String getAnimals_age() {
+        return animals_age;
     }
 
-    public void setAnimal_age(String animal_age) {
-        this.animal_age = animal_age;
+    public void setAnimals_age(String animals_age) {
+        this.animals_age = animals_age;
     }
 
     public int getId() {
