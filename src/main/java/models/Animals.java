aa0c0f56dc352887a351a96dded30498117532e2
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 11:19:26 +0300

    deleted the dao folder since i am using interface

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -3,15 +3,14 @@ package models;
 import java.time.LocalDateTime;
 
 public class Animals {
-    private int id;
-    private String name;
-    private String health;
-    private String age;
-    private LocalDateTime createdAt;
+    private int animals_id;
+    private String animals_name;
+    private String animals_health;
+    private int animals_age;
 
     public Animals(int id, String name) {
-        this.id = id;
-        this.name = name;
+        this.animals_id = animals_id;
+        this.animals_name = animals_name;
         this.id = 0;
     }
 
@@ -19,34 +18,34 @@ public class Animals {
         return new Animals(1,"lion");
     }
 
-    public int getId() {
-        return id;
+    public int getAnimals_id() {
+        return animals_id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setAnimals_id(int animals_id) {
+        this.animals_id = animals_id;
     }
 
-    public String getName() {
-        return name;
+    public String getAnimals_name() {
+        return animals_name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setAnimals_name(String animals_name) {
+        this.animals_name = animals_name;
     }
 
-    public String getHealth() {
-        return health;
+    public String getAnimals_health() {
+        return animals_health;
     }
-    public void setHealth(String health) {
-        this.health = health;
+    public void setAnimals_health(String animals_health) {
+        this.animals_health = animals_health;
     }
 
-    public String getAge() {
-        return age;
+    public int getAnimals_age() {
+        return animals_age;
     }
-    public void setAge(String age) {
-        this.age = age;
+    public void setAnimals_age(int animals_age) {
+        this.animals_age = animals_age;
     }
 
 
