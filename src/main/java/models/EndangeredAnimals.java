024ad97db507994742e684add12ad694b863e684
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 14:13:16 +0300

    added getter method for animal age

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -13,5 +13,24 @@ public class EndangeredAnimals {
         this.animal_age = animal_age;
         this.id = 1;
     }
+    public int getAnimal_id() {
+        return animal_id;
+    }
+
+    public void setAnimal_id(int animal_id) {
+        this.animal_id = animal_id;
+    }
+
+    public String getAnimal_name() {
+        return animal_name;
+    }
+
+    public void setAnimal_name(String animal_name) {
+        this.animal_name = animal_name;
+    }
+
+    public String getAnimal_health() {
+        return animal_health;
+    }
 
 }
