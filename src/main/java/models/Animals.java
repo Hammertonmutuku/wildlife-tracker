4e2f7fe2dd00cb21b186ae0a718352a83206d0ac
Hammertonmutuku <lthamma3@gmail.com>
2020-01-14 09:25:38 +0300

    added imports to the animals interface

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -1,4 +1,65 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Animals {
+    private String name;
+    private boolean endangered;
+    private LocalDateTime createdAt;
+    private int id;
+
+    public Animals(String description) {
+        this.endangered  = false;
+        this.name = name;
+        this.createdAt =LocalDateTime.now();
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animals)) return false;
+        Animals animals = (Animals) o;
+        return getEndangered() == animals.getEndangered()&&
+                getId() == animals.getId() &&
+                Objects.equals(getName(), animals.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getName(), getId());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompleted(boolean endangered) {
+        this.endangered = endangered;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean getEndangered(){
+        return this.endangered;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 }
+
+
--- src/test/java/models/AnimalsTest.java
+++ src/test/java/models/AnimalsTest.java
@@ -2,6 +2,9 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +12,34 @@ public class AnimalsTest {
 
     @Before
     public void setUp() throws Exception {
+    }
+
+    @Test
+    public void NewAnimalObjectCorrectlyCreated_true() throws Exception {
+        Animals animals = setupNewAnimals();
+        assertEquals(true, animals instanceof Animals);
+    }
+
+    @Test
+    public void AnimalNameCorrectlyCreated_true() throws Exception {
+        Animals animals = setupNewAnimals();
+        assertEquals(true, animals.getName());
+    }
+    @Test
+    public void isCompletedPropertyIsFalseAfterInstantiation() throws Exception {
+        Animals animals = setupNewAnimals();
+        assertEquals(false, animals.getEndangered()); //should never start as completed
+    }
+
+    @Test
+    public void getCreatedAtInstantiatesWithCurrentTimeToday() throws Exception {
+        Animals animals = setupNewAnimals();
+        assertEquals(LocalDateTime.now().getDayOfWeek(),animals.getCreatedAt().getDayOfWeek());
+    }
+
+    //helper methods
+    public Animals setupNewAnimals(){
+        return new Animals("Mow the lawn");
     }
 
     @After
